acbe027e8e11f5cc5ff5c05b1253e480df7bcf0d
lillfr <106988895+lillfr@users.noreply.github.com>
2025-08-15 23:59:39 +0300

    Create very simple calculatororo

---
+++ script.js
@@ -0,0 +1,139 @@
+function add(a, b) {
+    return a + b;
+}
+function substract(a, b) {
+    return a - b;
+}
+function multiply (a, b) {
+    return a * b;
+}
+function divide (a, b) {
+    return a / b;
+}
+
+function operate (a, b, op) {
+    a = +a;
+    b = +b;
+    let res;
+    switch (op) {
+        case '+': 
+            res = add(a, b);
+            break;
+        case '-': 
+            res = substract(a, b);
+            break;
+        case '*': 
+            res = multiply(a, b);
+            break;
+        case '÷': 
+            if (b == 0) return 'your calculator farted..'; 
+            res = divide(a, b);
+            break;
+    }
+    return +(res.toFixed(10)).toString()
+}
+
+function clearEverything() {
+    a = '';
+    b = '';
+    op = '';
+    text.innerHTML = '';
+    wasNum = false;
+    wasDot = false;
+    wasEqual = false;
+}
+
+let a = '';
+let b = '';
+let op = '';
+let wasNum = false;
+let wasEqual = false;
+let wasDot = false;
+const text = document.querySelector('#text');
+
+const digits = document.querySelectorAll(".digit");
+digits.forEach(digit => {
+    digit.addEventListener('click', (e) => {
+        if (wasNum) {
+            wasNum = false;
+            text.innerHTML = '';
+        }
+        if (wasEqual) {
+            wasEqual = false;
+            a = text.innerHTML;
+            clearEverything();
+        }
+        text.innerHTML += e.target.innerHTML;
+        if (op == '') a += e.target.innerHTML;
+        else b += e.target.innerHTML;
+    });
+});
+
+const dot = document.querySelector(".dot");
+dot.addEventListener('click', (e) => {
+    if (!wasDot) {
+        wasDot = true;
+        text.innerHTML += '.';
+        if (op == '') a += e.target.innerHTML;
+        else b += e.target.innerHTML;
+    }
+});
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach(operator => {
+    operator.addEventListener('click', (e) => {
+        wasDot = false;
+        if (a == '') {
+            a = '0';
+            text.innerHTML = '0';
+            wasNum = true;
+        }
+        if (a != '' && b != '') {
+            text.innerHTML = operate(a, b, op);
+            a = text.innerHTML;
+            b = '';
+        }
+        if (wasEqual) {
+            wasEqual = false;
+            a = text.innerHTML;
+            b = '';
+        }
+        if (e.target.innerHTML == '&divide;') op = '/';
+        else op = e.target.innerHTML;
+        wasNum = true;
+    });
+});
+
+const clear = document.querySelector("#clear");
+clear.addEventListener('click', (e) => {
+    clearEverything();
+});
+
+const del = document.querySelector("#del");
+del.addEventListener('click', (e) => {
+    let str = text.innerHTML;
+    str = str.slice(0, str.length-1);
+    text.innerHTML = str;
+    if (wasEqual) {
+        wasEqual = false;
+        a = str;
+        b = '';
+        op = '';
+    } else {
+        a = str;
+        op = '';
+    }
+});
+
+const equal = document.querySelector('#equal');
+equal.onclick = () => {
+    wasDot = false;
+    if (a == '') {
+        text.innerHTML = '0';
+    } else if (b == '') {
+        text.innerHTML = a;
+    } else {
+        text.innerHTML = operate(a, b, op);
+    }
+    wasEqual = true;
+};
